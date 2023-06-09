91e283d59d4b061fd7baf5d59f272439f59859b0
TamasSomi <>
2023-06-09 11:46:49 +0000

    Write a basic class to create the grid

--- run.py
+++ run.py
@@ -1,1 +1,20 @@
+class Grid:
+    def __init__(self):
+        self.grid = [["*"] * 4 for _ in range(4)]
 
+    def ship(self, row, col):
+        return self.grid[row][col]
+
+    def guess(self, row, col):
+        return self.grid[row][col]
+
+    def print_grid(self):
+        for row in self.grid:
+            for col in row:
+                print(col, end=" ")
+            print()
+
+
+
+grid = Grid()
+grid.print_grid()
