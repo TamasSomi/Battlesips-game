3cc0bc66c4dd83cd1853744e4beb0cb5aa298d07
TamasSomi <>
2023-06-13 09:36:42 +0000

    Expand the grid and the num of ships

--- run.py
+++ run.py
@@ -1,10 +1,14 @@
 import random
 
+# index = 0 player, index 1 = computer
+points = [0, 0]
+
 
 class Grid:
     """
     A class to create the board and place the ship.
     """
+
     def __init__(self, size, num_ships, name, type):
         self.size = size
         self.grid = [["*"] * size for _ in range(size)]
@@ -23,17 +27,21 @@ class Grid:
         print()
 
     def place_ship(self):
-        while True:
+        self.ships = []
+        ships_to_place = computers_grid.num_ships
+        while ships_to_place > 0:
             x = random.randint(0, self.size - 1)
             y = random.randint(0, self.size - 1)
 
+            if (x, y) in self.ships:
+                continue
+
             if self.grid[x][y] == "*":
-                self.ships = []
                 self.ships.append((x, y))
                 if self.type == "Player":
                     self.grid[x][y] = "S"
-                break
-        return (x, y)
+                ships_to_place -= 1
+        return self.ships
 
 
 print(30*"_")
@@ -45,13 +53,14 @@ while not len(player_name) > 2:
     print("Please provide a name (min 3 characters): ")
     player_name = input("What is your name?: ")
 
-players_grid = Grid(3, 1, player_name, type="Player")
-computers_grid = Grid(3, 1, "Computer", type="Computer")
+players_grid = Grid(4, 3, player_name, type="Player")
+computers_grid = Grid(4, 3, "Computer", type="Computer")
 players_grid.place_ship()
 players_grid.print_grid()
 computers_ship = computers_grid.place_ship()
 computers_grid.print_grid()
-print(f"Computer's ship: {computers_ship}")
+print(f"Computers ship: {computers_grid.ships}")
+print(f"players ship: {players_grid.ships}")
 size = players_grid.size
 
 
@@ -74,14 +83,18 @@ def take_input():
         take_input()
     else:
         players_grid.guesses.append(player_guess)
-    if computers_ship == player_guess:
+    if player_guess in computers_ship:
         computers_grid.grid[player_guess[0]][player_guess[1]] = "X"
         computers_grid.print_grid()
-        print("Congratulations You Won!🎉")
-        reset_game()
+        points[0] += 1
+        print("It's a hit!💥")
+        print(f"Scores: {player_name}: {points[0]}, Computer: {points[1]}")
     elif computers_grid.grid[player_guess[0]][player_guess[1]] == "*":
         computers_grid.grid[player_guess[0]][player_guess[1]] = "M"
         players_grid.guesses.append(player_guess)
+    if points[0] == 3:
+        print("Congratulations You Won!🎉")
+        reset_game()
     create_computer_guess()
 
 
@@ -98,12 +111,17 @@ def create_computer_guess():
     if players_grid.grid[computer_guess[0]][computer_guess[1]] == "S":
         players_grid.grid[computer_guess[0]][computer_guess[1]] = "X"
         players_grid.print_grid()
-        print("Ouups... the Computer Won!🎉")
-        reset_game()
+        points[1] += 1
+        print("The computer has a hit!💥")
+        print(f"Scores: {player_name}: {points[0]}, Computer: {points[1]}")
     elif players_grid.grid[computer_guess[0]][computer_guess[1]] == "*":
         players_grid.grid[computer_guess[0]][computer_guess[1]] = "M"
         computers_grid.guesses.append(computer_guess)
     print(f"Computer guessed: {computer_guess}")
+    if points[1] == 3:
+        print("Ouups... the Computer Won!🎉")
+        reset_game()
+
     players_grid.print_grid()
     computers_grid.print_grid()
     take_input()
@@ -114,11 +132,13 @@ def reset_game():
     new_game = input("Would you like to start a new game? yes/no ")
     if new_game.lower() == "yes":
         global computers_ship
+        points[0] = 0
+        points[1] = 0
         players_grid.guesses = []
         players_grid.ships = []
+        computers_grid.ships = []
         computers_ship = computers_grid.place_ship()
         computers_grid.guesses = []
-        computers_grid.ship = []
         players_grid.grid = [["*"] * size for _ in range(size)]
         computers_grid.grid = [["*"] * size for _ in range(size)]
         players_grid.place_ship()
@@ -135,7 +155,7 @@ def reset_game():
         print(30*"_")
         exit()
 
- 
+
 def start_game():
     take_input()
 
